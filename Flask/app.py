d2b96ce99c507bc4b79d4f6a059cf12a6450ee5a
GrandeSasuke <158407213+GrandeSasuke@users.noreply.github.com>
2026-09-09 18:25:15 -0300

    feat: testando aplicação com flask

--- Flask/app.py
+++ Flask/app.py
@@ -7,7 +7,8 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def main():
+def hello_world():
     return 'Hello World'
 
-app.run(debug=True)
+if __name__ == '__main__':
+    app.run(debug=True)
